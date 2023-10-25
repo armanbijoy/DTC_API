521426bf5a07cd898d85a629869d9775e7d3b1cd
armanbijoy <armanbijoy4@gmail.com>
2023-10-25 12:48:18 +0600

    Added

--- api/index.js
+++ api/index.js
@@ -29,16 +29,32 @@ app.get('/api/state', async (req, res) => {
   }
 });
 
+// app.get("/api/state/alberta", async (req, res) => {
+//   try {
+//     const albertaTest = await AlbertaQuestionModel.find({});
+//     res.status(200).json(albertaTest);
+//   } catch (err) {
+//     res.status(500).json({ message: err.message });
+//   }
+// });
+
 app.get("/api/state/alberta", async (req, res) => {
+  const page = parseInt(req.query.page) || 0;
+  const result = parseInt(req.query.result) || 10;
+  const skip = page * result;
+
   try {
-    const albertaTest = await AlbertaQuestionModel.find({});
-    res.status(200).json(albertaTest);
+    const questionList = await AlbertaQuestionModel.findOne({}).select('questionList')
+      .skip(skip)
+      .limit(result);
+
+    res.status(200).json(questionList.questionList);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 });
 
-// ... continue with your other routes
+
 
 mongoos.set("strictQuery", false);
 mongoos.connect(process.env.MONGO_URL).then(() => {
