8165026124228adc20dca86b8d046a88b485c61e
armanbijoy <armanbijoy4@gmail.com>
2023-10-26 11:09:49 +0600

    For All Questions

--- api/index.js
+++ api/index.js
@@ -29,15 +29,15 @@ app.get('/api/state', async (req, res) => {
   }
 });
 
-// app.get("/api/state/alberta", async (req, res) => {
-//   try {
-//     const albertaTest = await AlbertaQuestionModel.find({});
+app.get("/api/state/questions", async (req, res) => {
+  try {
+    const albertaTest = await AlbertaQuestionModel.find({});
     
-//     res.status(200).json(albertaTest);
-//   } catch (err) {
-//     res.status(500).json({ message: err.message });
-//   }
-// });
+    res.status(200).json(albertaTest);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 app.get("/api/state/alberta", async (req, res) => {
   const page = parseInt(req.query.page);
